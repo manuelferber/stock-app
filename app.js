d31f217b6b7d50d44d5c94c63286b9365af67e3d
Manuel Bernal <manuelbernal162019@gmail.com>
2022-06-05 20:53:14 -0500

    sacando modulos

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("peticion recibida");
+  res.status(200).send("<h1> hola mundo   mono</h1>  ");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Escuchando en puerto ${PORT}`);
+});
